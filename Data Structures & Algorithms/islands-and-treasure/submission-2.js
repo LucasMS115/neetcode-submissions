4bb5a4845d0c7d960b97f5c764acfbef4b2201d7
Lucas Mendes Sales <57019723+LucasMS115@users.noreply.github.com>
2026-05-16 17:34:31 -0300

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.js
@@ -0,0 +1,48 @@
+class Solution {
+    islandsAndTreasure(grid) {
+        const INF = 2147483647;
+
+        const rows = grid.length;
+        const cols = grid[0].length;
+
+        const queue = [];
+        let head = 0;
+
+        // Add all gates first
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === 0) {
+                    queue.push([r, c]);
+                }
+            }
+        }
+
+        const dirs = [
+            [1,0],
+            [-1,0],
+            [0,1],
+            [0,-1]
+        ];
+
+        while (head < queue.length) {
+            const [row, col] = queue[head];
+            head++;
+
+            for (const [dr, dc] of dirs) {
+                const nr = row + dr;
+                const nc = col + dc;
+
+                if (
+                    nr >= 0 && nr < rows &&
+                    nc >= 0 && nc < cols &&
+                    grid[nr][nc] === INF
+                ) {
+                    grid[nr][nc] = grid[row][col] + 1;
+                    queue.push([nr, nc]);
+                }
+
+                
+            }
+        }
+    }
+}
